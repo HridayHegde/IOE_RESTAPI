d080b852164dd40a4bacb7ca140cb0714762903d
Hriday Hegde <hridayhegde1999@gmail.com>
2021-05-18 09:34:07 +0530

    final commit

--- app.py
+++ app.py
@@ -1,32 +1,36 @@
 from flask import Flask, request
-#from flask import Flask, render_template, request, redirect, url_for, send_from_directory,jsonify
+from mysql.connector import connect, Error
+from datetime import datetime
 
+
+def setdata(datam,datetimem):
+    try:
+        with connect(host="den1.mysql3.gear.host ",user="ioeproj1",password="Os4KoJ7ZP!-z",database="ioeproj1",) as connection:
+            print(connection)
+            create_movies_table_query = "INSERT INTO gasdata(timedata,datafromsensor) values(%s,%s);" 
+            with connection.cursor() as cursor:
+                cursor.execute(create_movies_table_query,(str(datetimem),str(datam)))
+                connection.commit()
+
+    except Error as e:
+        print(e)
+    
 
 
 app = Flask(__name__)
 
-@app.route("/communicate", methods=['POST'])
+
+
+@app.route("/setdata", methods=['POST'])
 def communicate():
     some_json=request.get_json()
-    #sessionid = some_json['session']
-    #text = some_json['text']  
-    response = "YOLO"  
+    
+    data = some_json['data']
+    setdata(data, datetime.now())
+    response = "OK"  
     return str(response)
 
 if __name__=='__main__':
     app.run(debug=True)
+    #setdata(str(50),str(datetime.now().strftime("%m/%d/%Y, %H:%M:%S")))
 
-
-"""
-app = Flask(__name__)
-
-@app.route('/')
-def index():
-    
-    return render_template('index.html')
-
-
-if __name__ == '__main__':
-    app.run(host=0.0.0.0, port=8080, debug=True)
-
-"""
